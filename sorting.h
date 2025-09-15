ee6b805e4ffbf280159331a58689b45722b1cbef
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-14 21:07:08 -0600

    Add header file for bubble sort algorithm

---
+++ sorting.h
@@ -0,0 +1,10 @@
+#ifndef SORTING_H
+#define SORTING_H
+
+#include <vector>
+
+// Bubble Sort
+// Compara e intercambia pares adyacentes repetidamente hasta que la lista está ordenada.
+void bubbleSort(std::vector<int> &arr);
+
+#endif // SORTING_H
