c1b5372e1f9a9a35aa142d52daeccf7985b3e2fb
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-14 21:20:38 -0600

    Remove std:: prefix from vector type in sorting header

--- sorting.h
+++ sorting.h
@@ -1,14 +1,15 @@
 #ifndef SORTING_H
 #define SORTING_H
+using namespace std;
 
 #include <vector>
 
 // Bubble Sort
 // Compara e intercambia pares adyacentes repetidamente hasta que la lista está ordenada.
-void bubbleSort(std::vector<int> &arr);
+void bubbleSort(vector<int> &arr);
 
 // Selection Sort
 // Selecciona el elemento mínimo de la parte no ordenada y lo coloca en la posición correcta.
-void selectionSort(std::vector<int> &arr);
+void selectionSort(vector<int> &arr);
 
 #endif // SORTING_H
