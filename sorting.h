df61ef2165750bf677b013bf658668154e35283c
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-14 21:07:21 -0600

    Add selection sort function to sorting header

--- sorting.h
+++ sorting.h
@@ -7,4 +7,8 @@
 // Compara e intercambia pares adyacentes repetidamente hasta que la lista está ordenada.
 void bubbleSort(std::vector<int> &arr);
 
+// Selection Sort
+// Selecciona el elemento mínimo de la parte no ordenada y lo coloca en la posición correcta.
+void selectionSort(std::vector<int> &arr);
+
 #endif // SORTING_H
