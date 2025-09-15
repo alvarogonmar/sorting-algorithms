e4b0ca1867c3b3089cdca5aff8cec9b542b31559
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-14 21:32:55 -0600

    Implement bubble sort algorithm in sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -1,5 +1,19 @@
 #include "sorting.h"
 
-void bubbleSort(vector<int> &arr) {}
+void bubbleSort(vector<int> &arr)
+{
+    for (int i = arr.size() - 1; i > 0; --i)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 
 void selectionSort(vector<int> &arr) {}
