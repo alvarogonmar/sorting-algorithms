99130bc8b6ff6ac23245ca2187c644355e5f84ef
alvarogonmar <alvarogonmar0@gmail.com>
2025-09-17 17:53:58 -0600

    Enhance sorting algorithms documentation and fix insertion sort logic

--- sorting.cpp
+++ sorting.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+// Algoritmo Bubble Sort
+// Recorre el arreglo varias veces, comparando e intercambiando elementos adyacentes si están en el orden incorrecto.
 void bubbleSort(vector<int> &arr)
 {
     for (int i = arr.size() - 1; i > 0; --i)
@@ -22,6 +24,8 @@ void bubbleSort(vector<int> &arr)
     }
 }
 
+// Algoritmo Selection Sort
+// Busca el elemento mínimo en la parte no ordenada y lo coloca en la posición correspondiente al inicio del arreglo.
 void selectionSort(vector<int> &arr)
 {
     for (int i = 0; i < arr.size() - 1; i++)
@@ -43,14 +47,16 @@ void selectionSort(vector<int> &arr)
     }
 }
 
+// Algoritmo Insertion Sort
+// Inserta cada elemento en la posición correcta dentro de la parte ya ordenada del arreglo.
 void insertionSort(vector<int> &arr)
 {
     for (int i = 1; i < arr.size(); i++)
     {
         int temp = arr[i];
         int j = i - 1;
-
-        while (temp < arr[j] && j > -1)
+        // Desplaza los elementos mayores que temp hacia la derecha
+        while (j >= 0 && temp < arr[j])
         {
             arr[j + 1] = arr[j];
             arr[j] = temp;
