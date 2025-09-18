4259ac1df90078254590e94d788097ee9f2a0a64
Daniel Hernández <dani.hernandez.guti@gmail.com>
2025-09-17 20:32:36 -0600

    Add comments in QuickSort Algorithm

--- sorting.cpp
+++ sorting.cpp
@@ -130,15 +130,19 @@ void quickSort(vector<int> &arr, int low, int high) {
     if (low < high) {
         int pivot = arr[high];
         int i = low - 1;
+
+        // Reorganizar los elementos según el pivote
         for (int j = low; j < high; j++) {
             if (arr[j] <= pivot) {
                 i++;
                 swap(arr[i], arr[j]);
             }
         }
+         // Colocar el pivote en su posición final
         swap(arr[i + 1], arr[high]);
         int pi = i + 1;
 
+         // Llamadas recursivas
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
